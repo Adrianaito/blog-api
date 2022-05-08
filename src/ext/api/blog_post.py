c865b02d935e84cc6c84b7fa5d201a65e3e4b8c6
Adriana Ito <ito.dri@gmail.com>
2022-05-08 15:00:32 +0900

    add endpoints for user and blogpost

--- src/ext/api/blog_post.py
+++ src/ext/api/blog_post.py
@@ -1,4 +1,135 @@
-from flask import Blueprint, render_template
+from flask import request, Blueprint, json, Response
+from firebase_admin import auth
+from flask_cors import cross_origin
 from flask import current_app
 
+from src.ext.auth import Auth
+from src.ext.db import UserModel, BlogPostModel, UserSchema, BlogPostSchema
+
 blogpost_bp = Blueprint('blogpost', __name__)
+
+
+
+@cross_origin()
+@blogpost_bp.route('/blogpost', methods=['POST'])
+# @limiter.limit("5 per day")
+@Auth.check_token
+def create():
+  """
+  Takes in title and contents fields from the request and
+  creates a blog post.
+  Title and contents are required fields.
+  """
+  req_data = request.get_json()
+  jwt_token = request.headers.get('Authorization')
+  decoded_token = auth.verify_id_token(jwt_token)
+  email = decoded_token['email']
+  user = UserModel.get_user_by_email(email)
+  req_data['owner_id'] = user.id
+
+  blogpost_schema = BlogPostSchema()
+  data = blogpost_schema.load(req_data)
+
+  post = BlogPostModel(data)
+  post.save()
+  data = blogpost_schema.dump(post)
+  return custom_response(data, 201)
+
+
+@cross_origin()
+@blogpost_bp.route('/blogposts', methods=['GET'])
+# @limiter.limit("5 per day")
+def get_all():
+  """
+  Get All Blogposts
+  """
+  posts = BlogPostModel.get_all_blogposts()
+  blogposts_schema = BlogPostSchema(many=True)
+  data = blogposts_schema.dump(posts)
+  return custom_response(data, 200)
+
+
+@cross_origin()
+@blogpost_bp.route('/blogposts/<int:blogpost_id>', methods=['GET'])
+# @limiter.limit("5 per day")
+def get_one(blogpost_id):
+  """
+  Get A Blogpost
+  """
+  post = BlogPostModel.get_one_blogpost(blogpost_id)
+  if not post:
+    return custom_response({'error': 'post not found'}, 404)
+  blogpost_schema = BlogPostSchema()
+  data = blogpost_schema.dump(post)
+  return custom_response(data, 200)
+
+
+@cross_origin()
+@blogpost_bp.route('/blogposts/<int:blogpost_id>', methods=['PUT'])
+# @limiter.limit("2 per day")
+@Auth.check_token
+def update(blogpost_id):
+  """
+  Update A Blogpost.
+  """
+  req_data = request.get_json()
+  post = BlogPostModel.get_one_blogpost(blogpost_id)
+
+  jwt_token = request.headers.get('Authorization')
+  decoded_token = auth.verify_id_token(jwt_token)
+  email = decoded_token['email']
+  user = UserModel.get_user_by_email(email)
+  req_data['owner_id'] = user.id
+  blogpost_schema = BlogPostSchema()
+
+  if not post:
+    return custom_response({'error': 'post not found'}, 404)
+  data = blogpost_schema.dump(post)
+  if data.get('owner_id') != user.id:
+    return custom_response({'error': 'permission denied'}, 400)
+
+  data = blogpost_schema.load(req_data, partial=True)
+
+  post.update(data)
+
+  data = blogpost_schema.dump(post)
+  return custom_response(data, 200)
+
+
+@cross_origin()
+@blogpost_bp.route('/blogposts/<int:blogpost_id>', methods=['DELETE'])
+# @limiter.limit("2 per day")
+@Auth.check_token
+def delete(blogpost_id):
+  """
+  Delete A Blogpost
+  """
+  jwt_token = request.headers.get('Authorization')
+  decoded_token = auth.verify_id_token(jwt_token)
+  email = decoded_token['email']
+  user = UserModel.get_user_by_email(email)
+  post = BlogPostModel.get_one_blogpost(blogpost_id)
+
+  blogpost_schema = BlogPostSchema()
+
+  if not post:
+    return custom_response({'error': 'post not found'}, 404)
+  data = blogpost_schema.dump(post)
+  if data.get('owner_id') != user.id:
+    return custom_response({'error': 'permission denied'}, 400)
+  try:
+    post.delete()
+    return custom_response({'message': 'Post deleted!'}, 204)
+  except Exception as e:
+    return custom_response({'message': 'Something went wrong!'}, 400)
+
+
+def custom_response(res, status_code):
+  """
+  Custom Response Function
+  """
+  return Response(
+      mimetype="application/json",
+      response=json.dumps(res),
+      status=status_code
+  )
--- src/ext/api/user.py
+++ src/ext/api/user.py
@@ -1,5 +1,158 @@
-from flask import Blueprint, render_template
+from flask import request, json, Response, Blueprint, jsonify
 from flask_cors import cross_origin
 from flask import current_app
+from firebase_admin import credentials, auth
+import firebase_admin
+import pyrebase
+
+from src.ext.auth import Auth
+from src.ext.db import UserModel, BlogPostModel,  BlogPostSchema, UserSchema
+
 
 user_bp = Blueprint('user', __name__)
+
+
+cred = credentials.Certificate('fbAdminConfig.json')
+firebase = firebase_admin.initialize_app(cred)
+pb = pyrebase.initialize_app(json.load(open('fbconfig.json')))
+
+
+@user_bp.route('/', methods=['POST'])
+# @limiter.limit("2 per day")
+def create():
+  """
+  Create User Function
+  Saves User to db
+  Returns JWT token from firebase
+  """
+  req_data = request.get_json()
+#   data = user_schema.load(req_data)
+  password = req_data.get('password')
+  email = req_data.get('email')
+
+  # check if user already exist in the db
+  user_in_db = UserModel.get_user_by_email(email)
+  if user_in_db:
+    return custom_response({'error': 'User already exist, please supply another email address'}, 400)
+  try:
+    user = auth.create_user(
+        email=email,
+        password=password
+    )
+    user = UserModel(req_data)
+    user.save()
+    print("saving user")
+  except Exception as error:
+    print(error, type(error))
+    return custom_response({'message': "Email already exists"}, 400)
+
+#   user_data = user_schema.dump(user)
+  user_pb = pb.auth().sign_in_with_email_and_password(email, password)
+  jwt = user_pb['idToken']
+
+  return custom_response({'jwt_token': jwt}, 201)
+
+
+@cross_origin()
+@user_bp.route('/', methods=['GET'])
+# @limiter.limit("2 per day")
+@Auth.check_token
+def get_all():
+  '''
+  Gets all users from the database
+  '''
+  users = UserModel.get_all_users()
+  print(users)
+  user_schema = UserSchema(many=True)
+  all_users = user_schema.dump(users)
+  print("all users",all_users)
+  return jsonify(all_users, 200)
+
+
+@cross_origin()
+@user_bp.route('/login', methods=['POST'])
+# @limiter.limit("2 per day")
+def login():
+  '''
+  Takes in email and password and returns a JWT token
+  '''
+  req_data = request.get_json()
+#   data = user_schema.load(req_data, partial=True)
+  password = req_data.get('password')
+  email = req_data.get('email')
+
+  if not req_data.get('email') or not req_data.get('password'):
+    return custom_response({'error': 'you need email and password to sign in'}, 400)
+  user = UserModel.get_user_by_email(req_data.get('email'))
+
+  if not user:
+    return custom_response({'error': 'email not found'}, 400)
+
+#   user_data = user_schema.dump(user)
+  try:
+    user_pb = pb.auth().sign_in_with_email_and_password(email, password)
+    jwt = user_pb['idToken']
+  except:
+    return custom_response({'error': 'invalid credentials'}, 400)
+
+  return custom_response({'jwt_token': jwt}, 200)
+
+
+@cross_origin()
+@user_bp.route('/<int:user_id>', methods=['GET'])
+# @limiter.limit("2 per day")
+@Auth.check_token
+def get_a_user(user_id):
+  """
+  Get a single user
+  """
+  user = UserModel.get_one_user(user_id)
+  if not user:
+    return custom_response({'error': 'user not found'}, 404)
+  user_schema = UserSchema()
+  one_user = user_schema.dump(user)
+  return custom_response(one_user, 200)
+
+@cross_origin()
+@user_bp.route('/me', methods=['DELETE'])
+# @limiter.limit("2 per day")
+@Auth.check_token
+def delete():
+  """
+  Delete a user
+  """
+  jwt_token = request.headers.get('Authorization')
+  decoded_token = auth.verify_id_token(jwt_token)
+  email = decoded_token['email']
+  user = UserModel.get_user_by_email(email)
+  user.delete()
+  return custom_response({'message': 'User deleted successfully!'}, 204)
+
+
+@cross_origin()
+@user_bp.route('/me', methods=['GET'])
+# @limiter.limit("2 per day")
+@Auth.check_token
+def get_me():
+  """
+  Get me
+  """
+  jwt_token = request.headers.get('Authorization')
+  decoded_token = auth.verify_id_token(jwt_token)
+  email = decoded_token['email']
+
+  user = UserModel.get_user_by_email(email)
+  user_schema = UserSchema()
+  me_user = user_schema.dump(user)
+  return custom_response(me_user, 200)
+
+
+def custom_response(res, status_code):
+  """
+  Custom Response Function
+  """
+  return Response(
+      mimetype="application/json",
+      response=json.dumps(res),
+      status=status_code
+  )
